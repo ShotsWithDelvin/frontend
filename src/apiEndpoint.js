96b2523e3f68e7902eaf7783e11e7cceabf776b9
Delvin Reyes <DelvinReyes.95@Gmail.com>
2023-03-02 18:24:55 -0500

    test

--- src/apiEndpoint.js
+++ src/apiEndpoint.js
@@ -1,4 +1,4 @@
-let prodEndpoint = "https://backend-production-6bec.up.railway.app"
+let prodEndpoint = "https://backend-production-6bec.up.railway.app/users"
 let devEndpoint = "http://localhost:3001"
 
 let APIURL = process.env.NODE_ENV === "production" ? prodEndpoint : devEndpoint
